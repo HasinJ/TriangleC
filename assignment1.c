1f41d7068311734a445e39d8b4a5a1be41dbae65
HasinJ <hasinchoudhuryj@gmail.com>
2021-02-02 18:46:44 -0500

    fourth completed

--- assignment1.c
+++ assignment1.c
@@ -4,6 +4,8 @@
 
 void triangle(unsigned width)
 {
+  printf("\noriginal:");
+
     for (size_t i = 0; i <= width; i++) {
       for (size_t j = 0; j < i; j++) putchar('*');
       putchar('\n');
@@ -14,9 +16,17 @@ void v_triangle(unsigned width){
   printf("\nupside down:\n");
 
   for (size_t i = width; i>0; i--) {
-    for (size_t j = i; j>0; j--){
-      putchar('*');
-    }
+    for (size_t j = i; j>0; j--)  putchar('*');
+    putchar('\n');
+  }
+}
+
+void h_triangle(unsigned width){
+  printf("\nright side vertical:\n");
+
+  for (size_t i = width; i>0; i--) {
+    for (size_t k = width-i; k>0; k--) printf(" ");
+    for (size_t j = i; j>0; j--) putchar('*');
     putchar('\n');
   }
 }
@@ -24,4 +34,5 @@ void v_triangle(unsigned width){
 int main(int argc, char const *argv[argc+1]) {
   triangle(atoi(argv[1]));
   v_triangle(atoi(argv[1]));
+  h_triangle(atoi(argv[1]));
 }
