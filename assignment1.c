bd0349474d56bfc04e0326db69fc65792ed19906
HasinJ <hasinchoudhuryj@gmail.com>
2021-02-08 17:47:47 -0500

    added command line args

--- assignment1.c
+++ assignment1.c
@@ -4,7 +4,7 @@
 
 void triangle(signed width)
 {
-  printf("\noriginal:");
+  //printf("\noriginal:");
 
     for (size_t i = 0; i <= width; i++) {
       for (size_t j = 0; j < i; j++) putchar('*');
@@ -13,7 +13,7 @@ void triangle(signed width)
 }
 
 void v_triangle(signed width){
-  printf("\nupside down:\n");
+  //printf("\nupside down:\n");
 
   for (size_t i = width; i>0; i--) {
     for (size_t j = i; j>0; j--)  putchar('*');
@@ -22,7 +22,7 @@ void v_triangle(signed width){
 }
 
 void h_triangle(signed width){
-  printf("\nright side vertical:\n");
+  //printf("\nright side vertical:\n");
 
   for (size_t i = width; i>0; i--) {
     for (size_t k = width-i; k>0; k--) printf(" ");
@@ -33,12 +33,14 @@ void h_triangle(signed width){
 
 int main(int argc, char const *argv[argc+1]) {
   //printf("%d\n", argc);
-  if (argc!=2) return EXIT_FAILURE;
+  if (argc<2 || argc>3) return EXIT_FAILURE;
   signed width = atoi(argv[1]);
   if (width<1) return EXIT_FAILURE;
 
-  triangle(width);
-  v_triangle(width);
-  h_triangle(width);
+  if (argc==2) triangle(width);
+  else if (*argv[2]=='v') v_triangle(width);
+  else if (*argv[2]=='h') h_triangle(width);
+  else return EXIT_FAILURE;
+  
   return EXIT_SUCCESS;
 }
